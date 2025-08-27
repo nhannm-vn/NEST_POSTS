8c021c33da46e710cbb8fcca25c5b0bc7767be77
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-08-27 21:59:35 +0700

    auth route and register account

---
+++ src/routes/auth/auth.service.ts
@@ -0,0 +1,36 @@
+import { ConflictException, Injectable } from '@nestjs/common'
+import { Prisma } from '@prisma/client'
+import { HashingService } from 'src/shared/services/hashing.service'
+import { PrismaService } from 'src/shared/services/prisma.service'
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly hashingService: HashingService,
+    private readonly prismaService: PrismaService,
+  ) {}
+  //**register phải dùng async vì nó gọi đến các hàm bất đồng bộ
+  //**dùng async-await thì dùng try-catch
+  async register(body: any) {
+    try {
+      //hash password trước khi lưu vào db
+      const hashedPassword = await this.hashingService.hash(body.password)
+      //nhờ prisma service thực hiện
+      const user = await this.prismaService.user.create({
+        data: {
+          email: body.email,
+          password: hashedPassword,
+          name: body.name,
+        },
+      })
+      return user
+    } catch (error) {
+      //NestJS đã có sẵn ConflictException (HTTP 409) để báo lỗi trùng dữ liệu.
+      if (error instanceof Prisma.PrismaClientKnownRequestError && error.code === 'P2002') {
+        throw new ConflictException('Email already exists')
+      }
+      //500
+      throw error
+    }
+  }
+}
