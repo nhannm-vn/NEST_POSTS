316f153b637d52ca5ad73eb42f9c345c492434ab
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-09-04 21:49:59 +0700

    complete custom validator decorator

--- src/shared/decorators/custom-validator.decorator.ts
+++ src/shared/decorators/custom-validator.decorator.ts
@@ -15,6 +15,10 @@ export function Match(property: string, validationOptions?: ValidationOptions) {
           const relatedValue = (args.object as any)[relatedPropertyName]
           return value === relatedValue
         },
+        defaultMessage(args: ValidationArguments) {
+          const [relatedPropertyName] = args.constraints
+          return `${args.property} must match ${relatedPropertyName}`
+        },
       },
     })
   }
